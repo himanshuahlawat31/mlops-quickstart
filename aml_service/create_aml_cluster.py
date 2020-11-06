7d35134e5161c1683a193694154cd11654b9154c
BramCals <bram.cals@viqtordavis.com>
2020-11-06 11:33:41 +0100

    increased compute size

--- aml_service/create_aml_cluster.py
+++ aml_service/create_aml_cluster.py
@@ -11,31 +11,39 @@ print("In create_aml_cluster.py")
 print("Azure ML SDK version:", azureml.core.VERSION)
 
 parser = argparse.ArgumentParser("create_aml_cluster")
-parser.add_argument("--aml_compute_target", type=str, help="compute target name", dest="aml_compute_target", required=True)
+parser.add_argument(
+    "--aml_compute_target",
+    type=str,
+    help="compute target name",
+    dest="aml_compute_target",
+    required=True,
+)
 parser.add_argument("--path", type=str, help="path", dest="path", required=True)
 args = parser.parse_args()
 
 print("Argument 1: %s" % args.aml_compute_target)
 print("Argument 2: %s" % args.path)
 
-print('creating AzureCliAuthentication...')
+print("creating AzureCliAuthentication...")
 cli_auth = AzureCliAuthentication()
-print('done creating AzureCliAuthentication!')
+print("done creating AzureCliAuthentication!")
 
-print('get workspace...')
+print("get workspace...")
 ws = Workspace.from_config(path=args.path, auth=cli_auth)
-print('done getting workspace!')
+print("done getting workspace!")
 
 try:
     aml_compute = AmlCompute(ws, args.aml_compute_target)
     print("found existing compute target.")
 except ComputeTargetException:
     print("creating new compute target")
-    
-    provisioning_config = AmlCompute.provisioning_configuration(vm_size = "STANDARD_D2_V2",
-                                                                min_nodes = 1, 
-                                                                max_nodes = 1)    
+
+    provisioning_config = AmlCompute.provisioning_configuration(
+        vm_size="STANDARD_DS4_V2", min_nodes=1, max_nodes=8
+    )
     aml_compute = ComputeTarget.create(ws, args.aml_compute_target, provisioning_config)
-    aml_compute.wait_for_completion(show_output=True, min_node_count=None, timeout_in_minutes=20)
-    
+    aml_compute.wait_for_completion(
+        show_output=True, min_node_count=None, timeout_in_minutes=20
+    )
+
 print("Aml Compute attached")
